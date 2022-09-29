11c3c8098e5339871ab9978380fa999fa31c5671
rboudrouss <r.boudrouss@gmail.com>
2022-09-30 01:04:09 +0200

    adding folder for report

--- main.py
+++ main.py
@@ -57,19 +57,5 @@ if __name__ == "__main__":
     print(nb_placerL(list(range(5))))
 
 """
-TME2:
-
-nb_tot: int = 1
-for i in range(1,len(LEN_B)+1):
-    nb_tot *= nb_placer(i)
-
-1) 120
-2) 140
-3) 160
-4) 160
-5) 180
-
-nombre de configuration : 7.74 x 10^10
-
 
 """
---
+++ rapport/01-rapport.md
@@ -0,0 +1,14 @@
+TME2:
+
+```py
+nb_tot: int = 1
+for i in range(1,len(LEN_B)+1):
+    nb_tot *= nb_placer(i)
+```
+1) 120
+2) 140
+3) 160
+4) 160
+5) 180
+
+nombre de configuration : 7.74 x 10^10
